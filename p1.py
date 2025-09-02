2b811657bf7d24e1de630d0d74df354b7c5a7903
PavanSurisetti <pavan1898235@gmail.com>
2025-09-02 22:08:46 +0530

    Code-11

---
+++ p1.py
@@ -0,0 +1,14 @@
+#1.	Write a program to find the largest element among three Numbers.
+n1=2
+n2=3
+n3=5
+if(n1>n2):
+    if(n1>n3):
+        print("N1 is big:")
+    else:
+        print("N3 is big")
+else:
+    if(n2>n3):
+        print("N2 is big:")
+    else:
+        print("N3 is big")
