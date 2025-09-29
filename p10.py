79ed5dc2ff1853a08e25815d0ef71180a8c3916f
PavanSurisetti <pavan1898235@gmail.com>
2025-09-29 22:16:45 +0530

    Code-20

---
+++ p10.py
@@ -0,0 +1,7 @@
+#4.	Write a program to check if the substring is present in a given string or not.
+s=input('enter a string: ')
+k=input('enter what do you want to search:')
+if k in s:
+    print('yes, it is there')
+else:
+    print('Nope!')
