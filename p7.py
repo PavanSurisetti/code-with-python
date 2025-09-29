2eae124b62003426df0ed2f2623dfa57c632faa1
PavanSurisetti <pavan1898235@gmail.com>
2025-09-29 22:14:54 +0530

    Code-17

---
+++ p7.py
@@ -0,0 +1,5 @@
+#2.	Write a program to define a function using default arguments.
+def data(name='satya',age=17):
+    print(f'my name is {name} and my age is :{age}')
+data()#default names
+data('pepper',34)
