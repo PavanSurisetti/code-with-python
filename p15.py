c4a2fee4221919dbf403ddbf3d17a288b5b7863e
PavanSurisetti <pavan1898235@gmail.com>
2025-10-05 21:12:26 +0530

    Code-25

---
+++ p15.py
@@ -0,0 +1,6 @@
+#3.	Write a program to check if a given key exists in a dictionary or not
+d=dict(name='Satya',age=14,rank=36000)
+if 'name' in d:
+    print("Yup! key exists")
+else:
+    print("Nope! key not exists")
