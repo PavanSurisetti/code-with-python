230a5576cb7f903457f2541bb2fa1dc4e2952f00
PavanSurisetti <pavan1898235@gmail.com>
2025-10-05 21:13:02 +0530

    Code-26

---
+++ p16.py
@@ -0,0 +1,5 @@
+#4.	Write a program to add a new key-value pair to an existing dictionary
+d=dict(name='Satya',age=14,rank=36000)
+print(f"before adding :{d}")
+d['phone']=8985986385
+print(f"after adding :{d}")
