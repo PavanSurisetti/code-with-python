505f91f59ea904a17bdf1a9d6f5c0b2269799ae4
PavanSurisetti <pavan1898235@gmail.com>
2025-10-05 21:11:41 +0530

    Code-23

---
+++ p13.py
@@ -0,0 +1,5 @@
+#1.	Write a program to create tuples (name, age, address, college) for at least two members and concatenate the tuples and print the concatenated tuples.
+t1=('name:shuri','age:30','address:wakanda','college:wakanda univerity')
+t2=('name:peter','age:30','address:queens','college:queens univerity')
+t3=t1+t2
+print(t3)
