5743dfd39ea3daccac767ea2b3b6fd10067f7f65
PavanSurisetti <pavan1898235@gmail.com>
2025-10-09 22:39:01 +0530

    Code-36

---
+++ Q9.py
@@ -0,0 +1,6 @@
+#3.	Write a program to find the length of the string without using any library functions.
+str=input("ENTER THE STRING:")
+count=0
+for i in str:
+    count+=1
+print(f"THE LENGTH OF{str} IS:{count}")
