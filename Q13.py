81baffcb72063894d1542a0d0b730f32a38d55eb
PavanSurisetti <pavan1898235@gmail.com>
2025-10-12 13:17:02 +0530

    Code-40

---
+++ Q13.py
@@ -0,0 +1,5 @@
+#1.	Write a program to create tuples (name, age, address, college) for at least two members and concatenate the tuples and print the concatenated tuples.
+t1=tuple(eval(input("ENTER THE DATA INTO THE TUPLE1:")))
+t2=tuple(eval(input("ENTER THE DATA INTO THE TUPLE2:")))
+t3=t1+t2
+print(t3)
