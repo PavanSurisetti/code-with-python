c49af6a827e2656b1ecda8e262e26e29db88cd0b
PavanSurisetti <pavan1898235@gmail.com>
2025-10-18 21:08:11 +0530

    Code-48

---
+++ week3.1.py
@@ -0,0 +1,5 @@
+#1.	Write a program to create tuples (name, age, address, college) for at least two members and concatenate the tuples and print the concatenated tuples
+tuple1=("name:Shazam","age:18")
+tuple2=("address:vijayawada","college:Stanford university")
+final_tuple=tuple1+tuple2
+print(final_tuple)
